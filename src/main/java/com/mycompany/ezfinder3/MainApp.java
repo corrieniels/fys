82ec13b8fb8d123bfbb24c705a5efeb58772752b
corrieniels <34060753+corrieniels@users.noreply.github.com>
2017-12-04 13:15:28 +0100

    menu fix

--- src/main/java/com/mycompany/ezfinder3/MainApp.java
+++ src/main/java/com/mycompany/ezfinder3/MainApp.java
@@ -29,7 +29,7 @@ public class MainApp extends Application {
         parentWindow = stage;
 
         borderPane = new BorderPane();
-
+        
         switchScherm("fxml/login.fxml");
         
         Scene scene1 = new Scene(borderPane);
