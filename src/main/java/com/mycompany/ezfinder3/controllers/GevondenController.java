e2862af324946ee5f8473d9a03c555c5aeb16c86
mojobobopoker91 <34060761+mojobobopoker91@users.noreply.github.com>
2017-12-05 14:29:04 +0100

    gevonden aanpassingen

--- src/main/java/com/mycompany/ezfinder3/controllers/GevondenController.java
+++ src/main/java/com/mycompany/ezfinder3/controllers/GevondenController.java
@@ -1,22 +1,70 @@
 package com.mycompany.ezfinder3.controllers;
 
 import com.mycompany.ezfinder3.MainApp;
+import com.mycompany.ezfinder3.MyJDBC;
 import java.io.IOException;
 import java.net.URL;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.*;
 
 public class GevondenController implements Initializable {
     
-    @FXML
+    @FXML TextField bijzonderheden;
+    @FXML TextField nummer;
+    @FXML ComboBox kleur;  
+    @FXML ComboBox vliegveld;
+    
+    private MyJDBC db;
+    private ResultSet resultSet;
+    
+    @FXML 
     private void verder(ActionEvent event) throws IOException {
-        MainApp.switchScherm("fxml/matches_gevonden.fxml");
+//        MainApp.switchScherm("fxml/matches_gevonden.fxml");
+        String bijzonder = bijzonderheden.getText();
+        System.out.println(bijzonder);
+        
+        
+        String num = nummer.getText();
+        System.out.println(num);
+        
+        String color = kleur.getSelectionModel().getSelectedItem().toString();
+        System.out.println(color);
+        
     }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        
+        db = new MyJDBC();
+        
+        try {
+            
+            resultSet = db.executeResultSetQuery("SELECT * FROM vliegveld");
+            
+            while ( resultSet.next() ){
+                vliegveld.getItems().addAll(
+                      resultSet.getString("naam")
+                );
+            }
+            
+            resultSet = db.executeResultSetQuery("SELECT * FROM vertaling WHERE categorie = 'kleur' AND taal_id = 1");
+            
+            while ( resultSet.next() ){
+                kleur.getItems().addAll(
+                      resultSet.getString("tekst")
+                );
+            }
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(GevondenController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
     }
 }
