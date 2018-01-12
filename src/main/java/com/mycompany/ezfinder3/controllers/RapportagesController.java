5ca50dac5ad2fdb24789c5f2352d99037a95d4ec
corrieniels <34060753+corrieniels@users.noreply.github.com>
2018-01-12 17:19:34 +0100

    rapportage uit db

--- src/main/java/com/mycompany/ezfinder3/controllers/RapportagesController.java
+++ src/main/java/com/mycompany/ezfinder3/controllers/RapportagesController.java
@@ -34,7 +34,7 @@ public class RapportagesController implements Initializable {
     private MyJDBC db;
     private ResultSet resultSet;
 
-    private void loadData(AnchorPane ap, String titel) {
+    private void loadData(AnchorPane ap, String titel) throws SQLException {
 
         ap.getChildren().clear();
         CategoryAxis xAxis = new CategoryAxis();
@@ -43,24 +43,52 @@ public class RapportagesController implements Initializable {
         yAxis.setLabel("Aantal");
         BarChart bc = new BarChart(xAxis, yAxis);
         bc.setTitle(titel);
-        XYChart.Series series = new XYChart.Series();
-        series.setName("2003");
-        series.getData().add(new XYChart.Data("bc", 25601.34));
-        series.getData().add(new XYChart.Data("bc2", 201.34));
-        bc.getData().add(series);
+        
+        resultSet = db.executeResultSetQuery("SELECT MONTH(bs.datum) as maand, bs.status, COUNT(bs.status) as aantal FROM bagagestatus bs WHERE bs.datum = ( SELECT MAX(bs2.datum) FROM bagagestatus bs2 WHERE bs.bagagenummer = bs2.bagagenummer ) AND YEAR(bs.datum) = 2017 GROUP BY MONTH(bs.datum), bs.status ");
+        
+        while ( resultSet.next() ){
+            //resultSet.getInt("id");
+            
+            int maand = resultSet.getInt("maand");
+            int status = resultSet.getInt("status");
+            int aantal = resultSet.getInt("aantal");
+            
+            String strMaand = String.valueOf(maand);
+
+            
+            XYChart.Series series = new XYChart.Series();
+            series.setName( strMaand );
+            series.getData().add( new XYChart.Data(strMaand, aantal) );
+            bc.getData().add(series);
+
+            
+        }
+        
+//        XYChart.Series series = new XYChart.Series();
+//        series.setName("test");
+//        series.getData().add(new XYChart.Data("bc", 25601.34));
+//        series.getData().add(new XYChart.Data("bc2", 201.34));
+//        
+//        XYChart.Series series2 = new XYChart.Series();
+//        series2.setName("2005");
+//        series2.getData().add(new XYChart.Data("bc", 25601.34));
+//        series2.getData().add(new XYChart.Data("bc2", 201.34));
+//        
+//        bc.getData().addAll(series, series2);
+
         ap.getChildren().add(bc);
 
     }
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-
-        loadData(gevonden_koffers, "Gevonden koffers");
-        loadData(gematchte_koffers, "Gematchte koffers");
-        loadData(kosten, "Kosten");
-
+  
         db = new MyJDBC();
+
         try {
+            loadData(gevonden_koffers, "Gevonden koffers");
+            loadData(gematchte_koffers, "Gematchte koffers");
+            loadData(kosten, "Kosten");
 
             // vul naam combobox
             resultSet = db.executeResultSetQuery("SELECT * FROM vliegveld");
